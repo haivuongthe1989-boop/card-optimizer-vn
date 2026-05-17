86956316164c4810805810dd02a5468a378b9256
haivuongthe1989-boop <haivuongthe1989@gmail.com>
2026-05-17 21:08:06 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'card-optimizer-vn-v3-hide-calc';
+const CACHE_NAME = 'card-optimizer-vn-v5-ocr-many-bills';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./service-worker.js'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))); self.clients.claim(); });
